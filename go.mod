b3664777ad572440c5b0b53320e5df9fc05e15e2
Vivek Alhat <vivekalhat14@outlook.com>
2024-09-07 01:29:41 +0530

    add basic lb impl

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/VivekAlhat/load-balancer
+
+go 1.23.0
---
+++ main.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+	"sync"
+	"time"
+)
+
+type LoadBalancer struct {
+	Current int
+	Mutex   sync.Mutex
+}
+
+// round robin algorithm implementation to distribute load across servers
+func (lb *LoadBalancer) getNextServer(servers []*Server) *Server {
+	lb.Mutex.Lock()
+	defer lb.Mutex.Unlock()
+
+	for i := 0; i < len(servers); i++ {
+		idx := lb.Current % len(servers)
+		nextServer := servers[idx]
+		lb.Current++
+
+		nextServer.Mutex.Lock()
+		isHealthy := nextServer.IsHealthy
+		nextServer.Mutex.Unlock()
+
+		if isHealthy {
+			return nextServer
+		}
+	}
+
+	return nil
+}
+
+type Server struct {
+	URL       *url.URL
+	IsHealthy bool
+	Mutex     sync.Mutex
+}
+
+func (s *Server) ReverseProxy() *httputil.ReverseProxy {
+	return httputil.NewSingleHostReverseProxy(s.URL)
+}
+
+type Config struct {
+	Port                string   `json:"port"`
+	HealthCheckInterval string   `json:"healthCheckInterval"`
+	Servers             []string `json:"servers"`
+}
+
+func loadConfig(file string) (Config, error) {
+	var config Config
+
+	data, err := os.ReadFile(file)
+	if err != nil {
+		return config, err
+	}
+
+	err = json.Unmarshal(data, &config)
+	if err != nil {
+		return config, err
+	}
+
+	return config, nil
+}
+
+// health check function that runs in given interval to check health of servers
+func healthCheck(s *Server, healthCheckInterval time.Duration) {
+	for range time.Tick(healthCheckInterval) {
+		res, err := http.Head(s.URL.String())
+		s.Mutex.Lock()
+		if err != nil || res.StatusCode != http.StatusOK {
+			fmt.Printf("%s is down\n", s.URL)
+			s.IsHealthy = false
+		} else {
+			s.IsHealthy = true
+		}
+		s.Mutex.Unlock()
+	}
+}
+
+func main() {
+	config, err := loadConfig("config.json")
+	if err != nil {
+		log.Fatalf("Error loading configuration: %s", err.Error())
+	}
+
+	healthCheckInterval, err := time.ParseDuration(config.HealthCheckInterval)
+	if err != nil {
+		log.Fatalf("Invalid health check interval: %s", err.Error())
+	}
+
+	var servers []*Server
+	for _, serverUrl := range config.Servers {
+		u, _ := url.Parse(serverUrl)
+		server := &Server{URL: u, IsHealthy: true}
+		servers = append(servers, server)
+		go healthCheck(server, healthCheckInterval)
+	}
+
+	lb := LoadBalancer{Current: 0}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		server := lb.getNextServer(servers)
+		if server == nil {
+			http.Error(w, "No healthy server available", http.StatusServiceUnavailable)
+			return
+		}
+
+		// adding this header just for checking from which server the request is being handled.
+		// this is not recommended from security perspective as we don't want to let the client know which server is handling the request.
+		w.Header().Add("X-Forwarded-Server", server.URL.String())
+		server.ReverseProxy().ServeHTTP(w, r)
+	})
+
+	log.Println("Starting load balancer on port", config.Port)
+	err = http.ListenAndServe(config.Port, nil)
+	if err != nil {
+		log.Fatalf("Error starting load balancer: %s\n", err.Error())
+	}
+}
